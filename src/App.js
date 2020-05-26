8931251f8ad54a994b307ada9721c20f63e94994
Gauthamjm007 <j.mgautham15@gmail.com>
2020-05-26 22:06:47 +0530

    adding page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-const Home = lazy(() => import("./components/Home/CoronaCasesTable"));
-const Weather = lazy(() => import("./components/Weather/Weather"));
-const SignUp = lazy(() => import("./components/SignUp/SignUp"));
-const SignIn = lazy(() => import("./components/SignIn/SignIn"));
+import Home from "./components/Home/CoronaCasesTable";
+import Weather from "./components/Weather/Weather";
+import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+// const Home = lazy(() => import("./components/Home/CoronaCasesTable"));
+// const Weather = lazy(() => import("./components/Weather/Weather"));
+// const SignUp = lazy(() => import("./components/SignUp/SignUp"));
+// const SignIn = lazy(() => import("./components/SignIn/SignIn"));
 
 function App() {
   return (
     <>
-      <Suspense
+      {/* <Suspense
         fallback={
           <div className="spinner-border text-primary" role="status">
             <span className="sr-only">Loading...</span>
           </div>
         }
-      >
-        <Router>
-          <Switch>
-            <Route component={SignIn} path="/signin" exact />
-            <Route component={Home} path="/home" exact />
-            <Route component={Weather} path="/weather/state/:name" exact />
-            <Route component={SignUp} path="/" />
-          </Switch>
-        </Router>
-      </Suspense>
+      > */}
+      <Router>
+        <Switch>
+          <Route component={SignIn} path="/signin" exact />
+          <Route component={Home} path="/home" exact />
+          <Route component={Weather} path="/weather/state/:name" exact />
+          <Route component={SignUp} path="/" />
+        </Switch>
+      </Router>
+      {/* </Suspense> */}
     </>
   );
 }
